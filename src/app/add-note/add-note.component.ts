2545cfabb8d75856183f11372d16346cf6f197cc
Mateusz Bednarz <118743173+bedicooper@users.noreply.github.com>
2024-02-20 11:21:45 +0100

    add-note component created

---
+++ src/app/add-note/add-note.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-add-note',
+    standalone: true,
+    templateUrl:'./add-note.component.html',
+    styleUrl: './add-note.component.css',
+})
+
+export class AddNoteComponent {}
