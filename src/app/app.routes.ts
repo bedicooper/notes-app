c17624e765a206557e2766a873fc37904a318616
Mateusz Bednarz <118743173+bedicooper@users.noreply.github.com>
2024-02-20 12:35:50 +0100

    added a route to note details with URLParam

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { NotesListComponent } from './notes-list/notes-list.component';
 import { AddNoteComponent } from './add-note/add-note.component';
+import { NoteDetailComponent } from './note-detail/note-detail.component';
 
 export const routes: Routes = [
     { path: '', component: NotesListComponent },
     { path: 'new', component: AddNoteComponent },
+    { path: 'note/:id', component: NoteDetailComponent },
 ];
