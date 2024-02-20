248c137df2cb9f9b445680437fd03682da29f5b2
Mateusz Bednarz <118743173+bedicooper@users.noreply.github.com>
2024-02-20 11:25:39 +0100

    FormGroup addded to add-note component

--- src/app/add-note/add-note.component.ts
+++ src/app/add-note/add-note.component.ts
@@ -1,10 +1,17 @@
 import { Component } from "@angular/core";
-
+import { ReactiveFormsModule } from "@angular/forms";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 @Component({
     selector: 'app-add-note',
     standalone: true,
-    templateUrl:'./add-note.component.html',
+    templateUrl: './add-note.component.html',
     styleUrl: './add-note.component.css',
+    imports: [ReactiveFormsModule],
 })
 
-export class AddNoteComponent {}
+export class AddNoteComponent {
+    addNoteForm = new FormGroup({
+        title: new FormControl('', Validators.required),
+        text: new FormControl(''),
+    });
+}
