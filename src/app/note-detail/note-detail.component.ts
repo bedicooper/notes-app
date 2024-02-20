adf8d22ce6e3df8bc68b2bae10d00fa803cf5bfa
Mateusz Bednarz <118743173+bedicooper@users.noreply.github.com>
2024-02-20 12:39:17 +0100

    added activeRoute to id URLParam in note-detail component

--- src/app/note-detail/note-detail.component.ts
+++ src/app/note-detail/note-detail.component.ts
@@ -1,12 +1,18 @@
-import { Component } from "@angular/core";
-import { RouterModule } from "@angular/router";
+import { Component, inject } from "@angular/core";
+import { Router, RouterModule, ActivatedRoute } from "@angular/router";
+import { NOTES } from "../../notes";
 
-@Component ({
-selector: "app-note-detail",
-standalone: true,
-templateUrl: './note-detail.component.html',
-styleUrl: './note-detail.component.css',
-imports: [RouterModule],
+@Component({
+    selector: "app-note-detail",
+    standalone: true,
+    templateUrl: './note-detail.component.html',
+    styleUrl: './note-detail.component.css',
+    imports: [RouterModule],
 })
 
-export class NoteDetailComponent {}
+export class NoteDetailComponent {
+    router = inject(Router);
+    activateRoute = inject(ActivatedRoute);
+    id = Number(this.activateRoute.snapshot.paramMap.get('id'));
+    note = NOTES.find((note) => note.id === this.id);
+};
