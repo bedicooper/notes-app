373a19486c6e6fe24922d2f9e0884b91cae5be3f
Mateusz Bednarz <118743173+bedicooper@users.noreply.github.com>
2024-02-20 11:51:02 +0100

    addNote method to modify NOTES

--- src/app/add-note/add-note.component.ts
+++ src/app/add-note/add-note.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { ReactiveFormsModule } from "@angular/forms";
 import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { NOTES } from "../../notes";
 @Component({
     selector: 'app-add-note',
     standalone: true,
@@ -14,4 +15,25 @@ export class AddNoteComponent {
         title: new FormControl('', Validators.required),
         text: new FormControl(''),
     });
+
+    addNote() {
+        let title = this.addNoteForm.value.title ?? '';
+        let text = this.addNoteForm.value.text ?? '';
+        
+        if (this.addNoteForm.valid) {
+          let ids = NOTES.map((note) => note.id);
+          let maxId = 0;
+          if (ids.length > 0) {
+            maxId = Math.max(...ids);
+          }
+          let newNote = {
+            id: maxId + 1,
+            title: title,
+            text: text,
+          };
+
+          NOTES.unshift(newNote);
+          this.addNoteForm.reset();
+        }
+    }
 }
