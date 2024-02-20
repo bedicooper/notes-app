9748ffc35655f7a3bfd9fa85012a1ceb7ecb279d
Mateusz Bednarz <118743173+bedicooper@users.noreply.github.com>
2024-02-20 12:21:38 +0100

    RouterModule and back button added to note-detail

--- src/app/note-detail/note-detail.component.ts
+++ src/app/note-detail/note-detail.component.ts
@@ -1,10 +1,12 @@
 import { Component } from "@angular/core";
+import { RouterModule } from "@angular/router";
 
 @Component ({
 selector: "app-note-detail",
 standalone: true,
 templateUrl: './note-detail.component.html',
 styleUrl: './note-detail.component.css',
+imports: [RouterModule],
 })
 
 export class NoteDetailComponent {}
