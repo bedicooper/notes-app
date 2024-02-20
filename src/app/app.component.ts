5faf9a218a2e5003f59b96bb229420db35af050b
Mateusz Bednarz <118743173+bedicooper@users.noreply.github.com>
2024-02-20 11:22:20 +0100

    add-note referenced in root component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { NotesListComponent } from './notes-list/notes-list.component';
+import { AddNoteComponent } from './add-note/add-note.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet, 
-    HeaderComponent, 
+    HeaderComponent,
+    AddNoteComponent, 
     NotesListComponent,
   ],
   templateUrl: './app.component.html',
