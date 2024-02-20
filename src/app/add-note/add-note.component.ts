92ecb4498f007f88b4b06772adf297e0e05ca045
Mateusz Bednarz <118743173+bedicooper@users.noreply.github.com>
2024-02-20 12:12:35 +0100

    added discard button, and routing on submit to add-note component

--- src/app/add-note/add-note.component.ts
+++ src/app/add-note/add-note.component.ts
@@ -1,39 +1,43 @@
-import { Component } from "@angular/core";
+import { Component, inject } from "@angular/core";
+import { Router, RouterModule } from "@angular/router";
 import { ReactiveFormsModule } from "@angular/forms";
 import { FormGroup, FormControl, Validators } from "@angular/forms";
 import { NOTES } from "../../notes";
 @Component({
-    selector: 'app-add-note',
-    standalone: true,
-    templateUrl: './add-note.component.html',
-    styleUrl: './add-note.component.css',
-    imports: [ReactiveFormsModule],
+  selector: 'app-add-note',
+  standalone: true,
+  templateUrl: './add-note.component.html',
+  styleUrl: './add-note.component.css',
+  imports: [ReactiveFormsModule, RouterModule],
 })
 
 export class AddNoteComponent {
-    addNoteForm = new FormGroup({
-        title: new FormControl('', Validators.required),
-        text: new FormControl(''),
-    });
+  router = inject(Router);
 
-    addNote() {
-        let title = this.addNoteForm.value.title ?? '';
-        let text = this.addNoteForm.value.text ?? '';
-        
-        if (this.addNoteForm.valid) {
-          let ids = NOTES.map((note) => note.id);
-          let maxId = 0;
-          if (ids.length > 0) {
-            maxId = Math.max(...ids);
-          }
-          let newNote = {
-            id: maxId + 1,
-            title: title,
-            text: text,
-          };
+  addNoteForm = new FormGroup({
+    title: new FormControl('', Validators.required),
+    text: new FormControl(''),
+  });
 
-          NOTES.unshift(newNote);
-          this.addNoteForm.reset();
-        }
+  addNote() {
+    let title = this.addNoteForm.value.title ?? '';
+    let text = this.addNoteForm.value.text ?? '';
+
+    if (this.addNoteForm.valid) {
+      let ids = NOTES.map((note) => note.id);
+      let maxId = 0;
+      if (ids.length > 0) {
+        maxId = Math.max(...ids);
+      }
+      let newNote = {
+        id: maxId + 1,
+        title: title,
+        text: text,
+      };
+
+      NOTES.unshift(newNote);
+      this.addNoteForm.reset();
+      this.router.navigateByUrl('');
     }
+  }
 }
