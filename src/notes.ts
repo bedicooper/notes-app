a6f0d2eb844b6cdecdc84c63fd316a1ea12f9b45
Mateusz Bednarz <118743173+bedicooper@users.noreply.github.com>
2024-02-21 14:37:41 +0100

    removed test notes form notes.ts

--- src/notes.ts
+++ src/notes.ts
@@ -4,15 +4,4 @@ export interface Note {
     text: string;
 }
 
-export const NOTES: Note[] = [
-    {
-        id: 1,
-        title: 'Test Note 1',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    },
-    {
-        id: 2,
-        title: 'Test Note 2',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    },
-];
+export const NOTES: Note[] = [];
